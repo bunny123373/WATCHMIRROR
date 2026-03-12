51b696bcd78e63630e76334bd9751872a4fb8277
Balu <balu@gmail.com>
2026-03-12 08:17:30 +0530

    Add tap to fullscreen with landscape rotation on mobile

--- components/players/VideoPlayer.tsx
+++ components/players/VideoPlayer.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { createPlayer } from "@videojs/react";
 import { VideoSkin, Video, videoFeatures } from "@videojs/react/video";
 import "@videojs/react/video/skin.css";
+import { useRef, useEffect } from "react";
 
 const Player = createPlayer({ features: videoFeatures });
 
@@ -9,11 +12,43 @@ interface VideoPlayerProps {
 }
 
 export function VideoPlayer({ src }: VideoPlayerProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    const handleTap = () => {
+      const videoEl = container.querySelector("video");
+      if (!videoEl) return;
+
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        container.requestFullscreen();
+        const orientation = screen.orientation as ScreenOrientation & { lock?: (orientation: string) => Promise<void> };
+        if (orientation && typeof orientation.lock === "function") {
+          orientation.lock("landscape").catch(() => {});
+        }
+      }
+    };
+
+    container.addEventListener("click", handleTap);
+    container.addEventListener("touchend", handleTap);
+
+    return () => {
+      container.removeEventListener("click", handleTap);
+      container.removeEventListener("touchend", handleTap);
+    };
+  }, []);
+
   return (
-    <Player.Provider>
-      <VideoSkin>
-        <Video src={src} playsInline />
-      </VideoSkin>
-    </Player.Provider>
+    <div ref={containerRef}>
+      <Player.Provider>
+        <VideoSkin>
+          <Video src={src} playsInline />
+        </VideoSkin>
+      </Player.Provider>
+    </div>
   );
 }
